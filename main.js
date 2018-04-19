1a2eb767d3eae5f70136686ae6b4f9f70f456f77
Joshua Fredrickson <uafredrickson@gmail.com>
2018-04-18 18:56:38 -0700

    functionality works, but need to refactor to work within spec.

---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const reader = require('./lib/reader.js');
+
+const essaysPath = `${__dirname}/data/essays.txt`;
+const hamletPath = `${__dirname}/data/hamlet.txt`;
+const macbethPath = `${__dirname}/data/macbeth.txt`;
+
+const printText = (text) => {
+  console.log(text);
+  console.log('---------------------------------------------------------------------------------------');
+};
+
+const files = [essaysPath, hamletPath, macbethPath];
+
+const readFileArrayAsync = (fileArray, currentIndex, callback) => {
+  if (currentIndex >= fileArray.length) {
+    return callback();
+  }
+  const currentFilePath = fileArray[currentIndex];
+  try {
+    return reader.textFileReaderAsync(currentFilePath, (file) => {
+      printText(file);
+      readFileArrayAsync(fileArray, currentIndex +1, callback);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  return undefined;
+};
+
+readFileArrayAsync(files, 0, () => console.log('All files have read'));
